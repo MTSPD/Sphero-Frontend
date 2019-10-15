c526071661c64a575f1cd0f273b050317fa65020
MTSPD <wangmingl@deakin.edu.au>
2019-10-16 09:55:21 +1100

    Merge boilerplate

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,16 +1,16 @@
 import { AccessToken, logout } from 'contexts/helpers'
-import { notify } from 'components'
+// import { notify } from 'components'
 import { axiosInstance } from './index';
 /**
  *  @errorHelper :  Function to return error StatusText.
  */
 const errorHelper = (error) => {
   if (error.response === undefined) {
-    notify("Network Error");
+    //notify("Network Error");
     return false;
   }
   if (error.response.statusText !== "") {
-    notify(error.response.statusText);
+    //notify(error.response.statusText);
     return false;
   }
 }
@@ -32,6 +32,21 @@ class API {
     if (typeof callback === "function") {
       callback()
     }
+  }
+  ballroll = (data, callback) => {
+    axiosInstance.post('control', data).then(response => {
+      return callback(true)
+    }).catch(error => {
+      errorHelper(error)
+    })
+  }  
+
+  balllight = (data, callback) => {
+    axiosInstance.post('color', data).then(response => {
+      return callback(true)
+    }).catch(error => {
+      errorHelper(error)
+    })
   }
 }
 const instance = new API();
--- src/views/dependants/Home/Home.js
+++ src/views/dependants/Home/Home.js
@@ -1,10 +1,36 @@
 import React from 'react'
 
+import { makeStyles } from '@material-ui/core/styles';
+import {API} from 'helpers';
+// import Button from '@material-ui/core/Button';
+// import Fab from '@material-ui/core/Fab';
+// import IconButton from '@material-ui/core/IconButton';
+// import AddIcon from '@material-ui/icons/Add';
+// import DeleteIcon from '@material-ui/icons/Delete';
+// import NavigationIcon from '@material-ui/icons/Navigation';
+// import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+
+
+// test = () => {
+//   console.log('123')
+// }
+
 export const Home = () => {
+  const useStyles = makeStyles(theme => ({
+    button: {
+      margin: theme.spacing(1),
+    },
+    input: {
+      display: 'none',
+    },
+  }));
+  
+  const classes = useStyles();
 
   return (
-    <div>
-      WELCOME to HOME
+    <div>      
+      <button variant="outlined" color="primary" className={classes.button} onClick={()=>{API.ballroll({dir:1})}}>Frontward</button>
+      <button variant="outlined" color="primary" className={classes.button} onClick={()=>{API.ballroll({dir:-1})}}>Backward</button>
     </div>
   )
 }
